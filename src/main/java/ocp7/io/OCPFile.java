1f6f021c23006bbd11802cc4a758e140d2bc9e9b
Kuznetsov Artem <artkuznetsov@yandex.ru>
2017-02-07 14:49:28 +0300

    fix

--- src/main/java/ocp7/io/OCPFile.java
+++ src/main/java/ocp7/io/OCPFile.java
@@ -1,29 +1,29 @@
 package ocp7.io;
 
 import java.io.File;
+import java.io.IOException;
 
 public class OCPFile {
-    public static void main(String... args) {
-        countDirFiles(new File("c:\\work2"));
-    }
+    public static void main(String... args) throws IOException {
 
-    public static void countDirFiles(File dir) {
-        if (dir.isDirectory()) {
-            int fileCount = 0;
-            int dirCount = 0;
-            String[] list = dir.list();
-            File item = null;
-            for (String listItem : list) {
-                item = new File(dir, listItem);
-                if (item.isFile())
-                    ++fileCount;
-                else if (item.isDirectory())
-                    ++dirCount;
-            }
-            System.out.println("File(s):" + fileCount);
-            System.out.println("Dir(s):" + dirCount);
-        } else {
-            throw new IllegalArgumentException("Not a directory");
-        }
+        File dirs = new File("C:\\usr\\code\\java");
+        System.out.println(dirs.mkdirs());
+
+        File file = new File(dirs, "MyText.txt");
+        if (!file.exists())
+            System.out.println(file.createNewFile());
+
+        System.out.println("getAbsolutePath()-> " + file.getAbsolutePath());
+        System.out.println("getAbsoluteFile()-> " + file.getAbsoluteFile());
+        System.out.println("getCanonicalPath()-> " + file.getCanonicalPath());
+        System.out.println("getCanonicalFile()-> " + file.getCanonicalFile());
+        System.out.println("getParentFile()-> " + file.getParentFile());
+        System.out.println("getName()-> " + file.getName());
+        System.out.println("getParent()-> " + file.getParent());
+        System.out.println("isAbsolute()-> " + file.isAbsolute());
+        System.out.println("toPath()-> " + file.toPath());
+        System.out.println("toString()-> " + file.toString());
+        System.out.println("toURI()-> " + file.toURI());
+        System.out.println("canExecute()-> " + file.canExecute());
     }
 }
--- src/main/java/ocp7/io/OCPFile2.java
+++ src/main/java/ocp7/io/OCPFile2.java
@@ -1,14 +1,29 @@
 package ocp7.io;
 
 import java.io.File;
-import java.io.IOException;
 
 public class OCPFile2 {
-    public static void main(String... args) throws IOException {
-        File dirs = new File("C:\\usr\\code\\java");
-        System.out.println(dirs.mkdirs());
-        File file = new File(dirs, "MyText.txt");
-        if (!file.exists())
-            System.out.println(file.createNewFile());
+    public static void main(String... args) {
+        countDirFiles(new File("c:\\work2"));
+    }
+
+    public static void countDirFiles(File dir) {
+        if (dir.isDirectory()) {
+            int fileCount = 0;
+            int dirCount = 0;
+            String[] list = dir.list();
+            File item = null;
+            for (String listItem : list) {
+                item = new File(dir, listItem);
+                if (item.isFile())
+                    ++fileCount;
+                else if (item.isDirectory())
+                    ++dirCount;
+            }
+            System.out.println("File(s):" + fileCount);
+            System.out.println("Dir(s):" + dirCount);
+        } else {
+            throw new IllegalArgumentException("Not a directory");
+        }
     }
 }
