6f2402200701b78ad45e8831329b04566d76754c
Kuznetsov Artem <artkuznetsov@yandex.ru>
2017-02-08 17:24:50 +0300

    fix

--- src/main/java/ocp7/nio/OCPDirectoryStream.java
+++ src/main/java/ocp7/nio/OCPDirectoryStream.java
@@ -5,22 +5,47 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Iterator;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 
 public class OCPDirectoryStream {
     public static void main(String args[]) throws IOException {
-        Path dir = Paths.get("c:\\!");
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
-            for (Path value : stream) {
-                System.out.println("> " + value + ":" + Files.isDirectory(value));
-            }
-        }
 
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.{txt,java}")) {
-            Iterator<Path> iterator = stream.iterator();
-            while (iterator.hasNext()) {
-                System.out.println(">> " + iterator.next());
+        listFiles(Paths.get("c:\\work"));
+    }
+
+    static void listFiles(Path path) throws IOException {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    System.out.println("> " + entry);
+                    listFiles(entry);
+                } else {
+                    System.out.println("\t> " + entry);
+                }
             }
         }
+    }
+
+    static List<Path> listFiles2(Path path) throws IOException {
+        Deque<Path> stack = new ArrayDeque<Path>();
+        final List<Path> files = new LinkedList<>();
+
+        stack.push(path);
+
+        while (!stack.isEmpty()) {
+            DirectoryStream<Path> stream = Files.newDirectoryStream(stack.pop());
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    stack.push(entry);
+                } else {
+                    files.add(entry);
+                }
+            }
+            stream.close();
+        }
+        return files;
     }
 }
