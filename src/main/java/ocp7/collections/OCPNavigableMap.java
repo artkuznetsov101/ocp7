86e511e330c01226369829ffd74d80380170c0d2
Kuznetsov Artem <artkuznetsov@yandex.ru>
2017-02-12 11:47:52 +0300

    fix

--- src/main/java/ocp7/collections/OCPNavigableMap.java
+++ src/main/java/ocp7/collections/OCPNavigableMap.java
@@ -5,25 +5,25 @@ import java.util.TreeMap;
 
 public class OCPNavigableMap {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		NavigableMap<String, String> mymap = new TreeMap<String, String>();
-		mymap.put("a", "apple");
-		mymap.put("b", "boy");
-		mymap.put("c", "cat");
-		mymap.put("aa", "apple1");
-		mymap.put("bb", "boy1");
-		mymap.put("cc", "cat1");
-		System.out.println(mymap);
+        NavigableMap<String, String> mymap = new TreeMap<String, String>();
+        mymap.put("a", "apple");
+        mymap.put("b", "boy");
+        mymap.put("c", "cat");
+        mymap.put("aa", "apple1");
+        mymap.put("bb", "boy1");
+        mymap.put("cc", "cat1");
+        System.out.println(mymap);
 
-		mymap.pollLastEntry();
-		mymap.pollFirstEntry();
-		System.out.println(mymap);
+        mymap.pollLastEntry();
+        mymap.pollFirstEntry();
+        System.out.println(mymap);
 
-		NavigableMap<String, String> tailmap = mymap.tailMap("bb", false);
-		System.out.println(tailmap);
+        NavigableMap<String, String> tailmap = mymap.tailMap("bb", false);
+        System.out.println(tailmap);
 
-		System.out.println(tailmap.pollFirstEntry());
-		System.out.println(mymap.size());
-	}
+        System.out.println(tailmap.pollFirstEntry());
+        System.out.println(mymap.size());
+    }
 }
